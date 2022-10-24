874d55fecfa91269a6b464e4cb1bab35c5e5f80f
Zahra <zahraamasoumi@gmail.com>
2022-10-25 00:11:20 +0330

    a console menu

---
+++ SparseMatrix-ds/src/main/java/org/example/model/Menu.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+public class Menu {
+    public static void showMenu() {
+        System.out.println("--- codes ---");
+        System.out.println("0. add node");
+        System.out.println("1. remove node");
+        System.out.println("2. search node");
+        System.out.println("3. update node");
+        System.out.println("4. print matrix");
+        System.out.println("5. save matrix in csv file");
+        System.out.println("6. read matrix from csv file");
+        System.out.println("7. create new matrix");
+        System.out.println("8. exit");
+    }
+}
